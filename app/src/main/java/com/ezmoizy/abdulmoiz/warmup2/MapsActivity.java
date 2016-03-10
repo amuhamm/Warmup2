7fdf3ab29ab355c0d015f5f8ca174f77773298ca
Abdul Moiz <ezmoizy11@gmail.com>
2016-03-09 22:20:51 -0500

    Added Map Activity.

--- app/src/main/java/com/ezmoizy/abdulmoiz/warmup2/MapsActivity.java
+++ app/src/main/java/com/ezmoizy/abdulmoiz/warmup2/MapsActivity.java
@@ -1,7 +1,7 @@
 package com.ezmoizy.abdulmoiz.warmup2;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,23 +18,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.content_menu);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -45,7 +35,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Marker mcmaster_marker = mMap.addMarker(new MarkerOptions().position(mcmaster).title("McMaster University"));
 
         // Move the camera instantly to location with a zoom of 17.
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mcmaster, 10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mcmaster, 17));
 
         //made a new marker centered on McMaster
         Marker mcmarker = mMap.addMarker(new MarkerOptions()
--- app/src/main/java/com/ezmoizy/abdulmoiz/warmup2/MenuActivity.java
+++ app/src/main/java/com/ezmoizy/abdulmoiz/warmup2/MenuActivity.java
@@ -1,10 +1,10 @@
 package com.ezmoizy.abdulmoiz.warmup2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -81,17 +82,31 @@ public class MenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        Intent explicitIntent;
+
         if (id == R.id.nav_camara) {
             // Handle the camera action
+            explicitIntent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(explicitIntent);
         } else if (id == R.id.nav_gallery) {
+            explicitIntent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(explicitIntent);
 
         } else if (id == R.id.nav_slideshow) {
+            explicitIntent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(explicitIntent);
 
         } else if (id == R.id.nav_manage) {
+            explicitIntent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(explicitIntent);
 
         } else if (id == R.id.nav_share) {
+            explicitIntent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(explicitIntent);
 
         } else if (id == R.id.nav_send) {
+            explicitIntent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(explicitIntent);
 
         }
 
